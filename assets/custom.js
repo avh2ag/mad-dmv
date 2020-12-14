5ca22fdc75028a5ded10dbc9541644dd52397831
Alex Harasty <alex.harasty@gmail.com>
2020-12-13 20:36:58 -0500

    initial copy over

---
+++ assets/custom.js
@@ -0,0 +1,9 @@
+/* Theme Name: nebhub - Responsive Website Template
+ * Author:nebhub
+ * Author URI:http://www.nebhub.com
+ * Author e-mail:nebhub.com@gmail.com
+ * Version:1.1.0
+ * Created:March 2015
+ * License URI:http://support.wrapbootstrap.com/
+ * File Description: Place here your custom scripts
+ */
